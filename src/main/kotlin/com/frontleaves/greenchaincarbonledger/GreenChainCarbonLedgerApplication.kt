8504f7ff7400006b16f581733071e14a79f1a5e0
XiaoLFeng <gm@x-lf.cn>
2024-02-18 15:52:33 +0800

    commit initial

---
+++ src/main/kotlin/com/frontleaves/greenchaincarbonledger/GreenChainCarbonLedgerApplication.kt
@@ -0,0 +1,11 @@
+package com.frontleaves.greenchaincarbonledger
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GreenChainCarbonLedgerApplication
+
+fun main(args: Array<String>) {
+    runApplication<GreenChainCarbonLedgerApplication>(*args)
+}
---
+++ src/test/kotlin/com/frontleaves/greenchaincarbonledger/GreenChainCarbonLedgerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.frontleaves.greenchaincarbonledger
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GreenChainCarbonLedgerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
