51de71f2f81029ba391a3940fbe2bb2f43a59ebe
XiaoLFeng <gm@x-lf.cn>
2024-02-27 17:12:29 +0800

    feat: 管理账号注册

--- src/main/kotlin/com/frontleaves/greenchaincarbonledger/mapper/UserMapper.java
+++ src/main/kotlin/com/frontleaves/greenchaincarbonledger/mapper/UserMapper.java
@@ -1,6 +1,7 @@
 package com.frontleaves.greenchaincarbonledger.mapper;
 
 import com.frontleaves.greenchaincarbonledger.models.doData.UserDO;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -15,4 +16,22 @@ import org.apache.ibatis.annotations.Select;
 public interface UserMapper {
     @Select("SELECT * FROM fy_carbon.fy_user WHERE uuid = #{uuid}")
     UserDO getUserByUuid(String uuid);
+
+    @Select("SELECT * FROM fy_carbon.fy_user WHERE user_name = #{username}")
+    UserDO getUserByUsername(String username);
+
+    @Select("SELECT * FROM fy_carbon.fy_user WHERE email = #{email}")
+    UserDO getUserByEmail(String email);
+
+    @Select("SELECT * FROM fy_carbon.fy_user WHERE phone = #{phone}")
+    UserDO getUserByPhone(String phone);
+
+    @Select("SELECT * FROM fy_carbon.fy_user WHERE real_name = #{realname}")
+    UserDO getUserByRealname(String realname);
+
+    @Insert("""
+        INSERT INTO fy_carbon.fy_user (uuid, user_name, real_name, email, phone, password)
+            VALUES (#{uuid}, #{userName}, #{realName}, #{email}, #{phone}, #{password})
+        """)
+    boolean createUser(UserDO newUserDO);
 }
