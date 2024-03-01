cdf34e64e6551d9f6e91235a2baf929041188408
XiaoLFeng <gm@x-lf.cn>
2024-03-01 13:47:15 +0800

    patch: 补丁

--- src/main/kotlin/com/frontleaves/greenchaincarbonledger/models/voData/getData/AuthOrganizeRegisterVO.java
+++ src/main/kotlin/com/frontleaves/greenchaincarbonledger/models/voData/getData/AuthOrganizeRegisterVO.java
@@ -1,5 +1,7 @@
 package com.frontleaves.greenchaincarbonledger.models.voData.getData;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
@@ -8,9 +10,13 @@ import lombok.Data;
  */
 @Data
 public class AuthOrganizeRegisterVO {
+    @NotBlank(message = "组织名称不能为空")
     String organize;
+    @Pattern(regexp = "^[a-zA-Z0-9_-]{4,40}$", message = "用户名格式不正确【用户名长度在4-40之间，只能包含字母、数字、下划线、减号】")
     String username;
+    @Pattern(regexp = "^(13[0-9]|14[01456879]|15[0-35-9]|16[2567]|17[0-8]|18[0-9]|19[0-35-9])\\d{8}$", message = "手机号格式不正确")
     String phone;
+    @Email(message = "邮箱格式不正确")
     String email;
     @Pattern(regexp = "^[a-zA-Z0-9]{6,10}$", message = "验证码不正确")
     String code;
