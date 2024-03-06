38cda9e0c6018fbc0aa9ac9ee6696b051c049c9c
DC_DC <3284155190@qq.com>
2024-03-06 14:50:35 +0800

    feat:添加角色信息

--- src/main/kotlin/com/frontleaves/greenchaincarbonledger/controllers/UserController.java
+++ src/main/kotlin/com/frontleaves/greenchaincarbonledger/controllers/UserController.java
@@ -1,5 +1,6 @@
 package com.frontleaves.greenchaincarbonledger.controllers;
 
+import com.frontleaves.greenchaincarbonledger.annotations.CheckAccountPermission;
 import com.frontleaves.greenchaincarbonledger.services.UserService;
 import com.frontleaves.greenchaincarbonledger.utils.BaseResponse;
 import com.frontleaves.greenchaincarbonledger.utils.ErrorCode;
@@ -8,10 +9,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
@@ -71,6 +69,13 @@ public class UserController {
         }
         // 业务操作
         return userService.getUserList(timestamp, request, type, search , limit, page, order);
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<BaseResponse> addAccount(
+            
+    ){
+        return null;
     }
 }
 
