7e714876a6166234b9cf05cf9b906878f17ff571
FLASHLACK1314 <zlp18021547586@163.com>
2024-03-13 22:47:46 +0800

    Feature:获取自己组织碳排放配额

--- src/main/kotlin/com/frontleaves/greenchaincarbonledger/services/impl/CarbonServiceImpl.java
+++ src/main/kotlin/com/frontleaves/greenchaincarbonledger/services/impl/CarbonServiceImpl.java
@@ -1,7 +1,13 @@
 package com.frontleaves.greenchaincarbonledger.services.impl;
 
+import com.frontleaves.greenchaincarbonledger.dao.CarbonDAO;
+import com.frontleaves.greenchaincarbonledger.models.doData.CarbonQuotaDO;
+import com.frontleaves.greenchaincarbonledger.models.voData.returnData.BackCarbonQuotaVO;
 import com.frontleaves.greenchaincarbonledger.services.CarbonService;
 import com.frontleaves.greenchaincarbonledger.utils.BaseResponse;
+import com.frontleaves.greenchaincarbonledger.utils.ResultUtil;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -10,6 +16,10 @@ import org.jetbrains.annotations.Nullable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * @author FLASHLACK
  */
@@ -17,10 +27,76 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class CarbonServiceImpl implements CarbonService {
-
+    private final CarbonDAO carbonDAO;
+    private final Gson gson;
     @NotNull
     @Override
     public ResponseEntity<BaseResponse> getOwnCarbonQuota(long timestamp, @NotNull HttpServletRequest request, @Nullable Integer start, @Nullable Integer end) {
-        return null;
+        log.info("[Service] 执行 getOwnCarbonQuota ");
+        String getUuid = request.getHeader("\"X-Auth-UUID\"");
+        SimpleDateFormat thisYear = new SimpleDateFormat("yyyy");
+        //检查数据
+        if (start == null){
+            start = Integer.parseInt(thisYear.format(System.currentTimeMillis()));
+        }
+        if (end == null){
+            end = Integer.parseInt(thisYear.format(System.currentTimeMillis()));
+        }
+        ArrayList<CarbonQuotaDO> newCarbonQuotaList = new ArrayList<>();
+        while (start<=end){
+            CarbonQuotaDO[] carbonQuotaArray = carbonDAO.getQuotaByUuidYear(getUuid,start);
+            newCarbonQuotaList.addAll(Arrays.asList(carbonQuotaArray));
+            start ++;
+        }
+        //整理数据
+        ArrayList<BackCarbonQuotaVO> backCarbonQuotaVOList = new ArrayList<>();
+        for (CarbonQuotaDO carbonQuota : newCarbonQuotaList){
+            BackCarbonQuotaVO backCarbonQuotaVO = new BackCarbonQuotaVO();
+            backCarbonQuotaVO.setUuid(carbonQuota.getUuid());
+            backCarbonQuotaVO.setOrganizeUuid(carbonQuota.organizeUuid);
+            backCarbonQuotaVO.setQuotaYear(carbonQuota.quotaYear);
+            backCarbonQuotaVO.setTotalQuota(carbonQuota.totalQuota);
+            backCarbonQuotaVO.setAllocatedQuota(carbonQuota.getAllocatedQuota());
+            backCarbonQuotaVO.setUsedQuota(carbonQuota.usedQuota);
+            backCarbonQuotaVO.setAllocationDate(carbonQuota.allocationDate);
+            backCarbonQuotaVO.setComplianceStatus(carbonQuota.complianceStatus);
+            backCarbonQuotaVO.setCreatedAt(carbonQuota.createdAt);
+            backCarbonQuotaVO.setUpdatedAt(carbonQuota.updatedAt);
+            backCarbonQuotaVO.setAuditLog(gson.fromJson(String.valueOf(carbonQuota.getAuditLog()), new TypeToken<ArrayList<String>>() {
+            }.getType()));
+            backCarbonQuotaVOList.add(backCarbonQuotaVO);
+        }
+        return ResultUtil.success(timestamp,backCarbonQuotaVOList);
+
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
