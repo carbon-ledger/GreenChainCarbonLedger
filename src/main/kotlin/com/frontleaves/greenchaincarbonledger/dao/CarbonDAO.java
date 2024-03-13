65eb407720f631227ac277d0c711ad0a7f2112b6
FLASHLACK1314 <zlp18021547586@163.com>
2024-03-13 20:04:31 +0800

    Feature:创建用于存放CarbonDAO

---
+++ src/main/kotlin/com/frontleaves/greenchaincarbonledger/dao/CarbonDAO.java
@@ -0,0 +1,16 @@
+package com.frontleaves.greenchaincarbonledger.dao;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Carbon的数据处理
+ * @author FLAHSLACK
+ */
+@Slf4j
+@Repository
+@RequiredArgsConstructor
+public class CarbonDAO {
+
+}
