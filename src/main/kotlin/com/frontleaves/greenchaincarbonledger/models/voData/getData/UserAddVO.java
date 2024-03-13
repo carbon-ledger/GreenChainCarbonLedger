7c086e8eaaed5876122d571165c1e6067a6f62f2
XiaoLFeng <gm@x-lf.cn>
2024-03-13 22:18:28 +0800

    patch: 代码修改

--- src/main/kotlin/com/frontleaves/greenchaincarbonledger/models/voData/getData/UserAddVO.java
+++ src/main/kotlin/com/frontleaves/greenchaincarbonledger/models/voData/getData/UserAddVO.java
@@ -24,4 +24,6 @@ public class UserAddVO {
     public String phone;
     @Email(message = "邮箱格式不正确")
     public String email;
+    @Pattern(regexp = "^(console|admin|organize)$", message = "角色格式不正确【角色只能是console、admin、organize】")
+    public String role;
 }
