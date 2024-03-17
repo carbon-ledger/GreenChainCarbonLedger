1a26a0c8cf8586440b8d7bab020228fb1f8cff83
DC_DC <3284155190@qq.com>
2024-03-17 13:41:39 +0800

    feat-获取自己组织的碳核算数据

--- src/main/kotlin/com/frontleaves/greenchaincarbonledger/services/impl/CarbonServiceimpl.java
+++ src/main/kotlin/com/frontleaves/greenchaincarbonledger/services/impl/CarbonServiceimpl.java
@@ -1,7 +1,13 @@
 package com.frontleaves.greenchaincarbonledger.services.impl;
 
+import com.frontleaves.greenchaincarbonledger.dao.CarbonAccountingDAO;
+import com.frontleaves.greenchaincarbonledger.models.doData.CarbonAccountingDO;
+import com.frontleaves.greenchaincarbonledger.models.voData.returnData.BackCarbonAccountingVO;
 import com.frontleaves.greenchaincarbonledger.services.CarbonService;
 import com.frontleaves.greenchaincarbonledger.utils.BaseResponse;
+import com.frontleaves.greenchaincarbonledger.utils.ErrorCode;
+import com.frontleaves.greenchaincarbonledger.utils.ProcessingUtil;
+import com.frontleaves.greenchaincarbonledger.utils.ResultUtil;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -10,10 +16,17 @@ import org.jetbrains.annotations.Nullable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author 32841
+ */
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class CarbonServiceimpl implements CarbonService {
+    private final CarbonAccountingDAO carbonAccountingDAO;
 
     @NotNull
     @Override
@@ -25,9 +38,39 @@ public class CarbonServiceimpl implements CarbonService {
 
     @NotNull
     @Override
-    public ResponseEntity<BaseResponse> getCarbonAccounting(long timestamp, @NotNull String limit, int page, @NotNull String order) {
+    public ResponseEntity<BaseResponse> getCarbonAccounting(long timestamp, @NotNull HttpServletRequest request, Integer limit, Integer page, String order) {
         log.info("[Service] 执行 getCarbonAccounting 方法");
-        
-        return null;
+        // 检查参数，如果未设置（即为null），则使用默认值
+        limit = (limit == null || limit > 100) ? 20 : limit;
+        page = (page == null) ? 1 : page;
+        if (order == null || order.isBlank()) {
+            order = "id ASC";
+        } else {
+            order = "id " + order;
+        }
+        // 获取自己企业的Uuid
+        String organizeUuid = ProcessingUtil.getAuthorizeUserUuid(request);
+        List<BackCarbonAccountingVO> carbonAccountinglist = new ArrayList<>();
+        List<CarbonAccountingDO> carbonAccountingDOList = carbonAccountingDAO.getCarbonAccountingList(organizeUuid, limit, page, order);
+        if (carbonAccountingDOList != null){
+            for (CarbonAccountingDO carbonAccountingDO : carbonAccountingDOList) {
+                BackCarbonAccountingVO backCarbonAccountingVO = new BackCarbonAccountingVO();
+                backCarbonAccountingVO
+                        .setId(carbonAccountingDO.getId())
+                        .setOrganizeUuid(carbonAccountingDO.getOrganizeUuid())
+                        .setEmissionSource(carbonAccountingDO.getEmissionSource())
+                        .setEmissionAmount(carbonAccountingDO.getEmissionAmount())
+                        .setAccountingPeriod(carbonAccountingDO.getAccountingPeriod())
+                        .setEmissionSource(carbonAccountingDO.getEmissionSource())
+                        .setDataVerificationStatus(carbonAccountingDO.getDataVerificationStatus())
+                        .setCreateAt(carbonAccountingDO.getCreateAt())
+                        .setUpdateAt(carbonAccountingDO.getUpdateAt());
+                carbonAccountinglist.add(backCarbonAccountingVO);
+            }
+            return ResultUtil.success(timestamp, "数据已准备完毕", carbonAccountinglist);
+        } else {
+            return ResultUtil.error(timestamp, ErrorCode.SELECT_DATA_ERROR);
+        }
+
     }
 }
