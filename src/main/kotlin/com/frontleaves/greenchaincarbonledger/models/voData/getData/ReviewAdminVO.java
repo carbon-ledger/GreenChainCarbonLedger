7343cda86811ba942a6367589ce7b21e71c625e4
XiaoLFeng <gm@x-lf.cn>
2024-03-18 01:20:57 +0800

    feat: 审核代码内容提交

---
+++ src/main/kotlin/com/frontleaves/greenchaincarbonledger/models/voData/getData/ReviewAdminVO.java
@@ -0,0 +1,26 @@
+package com.frontleaves.greenchaincarbonledger.models.voData.getData;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+/**
+ * ReviewAdminVO
+ * <hr/>
+ * 管理员审核VO，用于获取管理员审核信息，包括类型、组织名称、组织授权、法定代表人姓名、法定代表人身份证号、
+ * 法定代表人身份证正面、法定代表人身份证反面
+ *
+ * @since v1.0.0-SNAPSHOT
+ * @version v1.0.0-SNAPSHOT
+ * @author xiao_lfeng
+ */
+@Data
+@Accessors(chain = true)
+public class ReviewAdminVO {
+    public String type;
+    public String organizeName;
+    public String organizeAuthorize;
+    public String legalRepresentativeName;
+    public String legalRepresentativeId;
+    public String legalIdCardFront;
+    public String legalIdCardBack;
+}
---
+++ src/main/kotlin/com/frontleaves/greenchaincarbonledger/services/ReviewService.kt
@@ -0,0 +1,46 @@
+package com.frontleaves.greenchaincarbonledger.services
+
+import com.frontleaves.greenchaincarbonledger.models.voData.getData.ReviewOrganizeVO
+import com.frontleaves.greenchaincarbonledger.utils.BaseResponse
+import jakarta.servlet.http.HttpServletRequest
+import org.springframework.http.ResponseEntity
+
+/**
+ * ReviewService
+ *
+ * 用于实现审核服务, 用于组织账户与监管账户的实名认证审核, 以及组织账户的组织审核
+ *
+ * @since v1.0.0-SNAPSHOT
+ * @version v1.0.0-SNAPSHOT
+ * @author xiao_lfeng
+ */
+interface ReviewService {
+
+    /**
+     * addReviewFromOrganize
+     *
+     * 用于组织账户的实名认证审核
+     *
+     * @param reviewOrganizeVO ReviewOrganizeVO
+     * @param request HttpServletRequest
+     * @return ResponseEntity<BaseResponse>
+     */
+    fun addReviewFromOrganize(
+        reviewOrganizeVO: ReviewOrganizeVO,
+        request: HttpServletRequest
+    ): ResponseEntity<BaseResponse>
+
+    /**
+     * addReviewFromAdmin
+     *
+     * 用于组织账户的组织审核
+     *
+     * @param reviewOrganizeVO ReviewOrganizeVO
+     * @param request HttpServletRequest
+     * @return ResponseEntity<BaseResponse>
+     */
+    fun addReviewFromAdmin(
+        reviewOrganizeVO: ReviewOrganizeVO,
+        request: HttpServletRequest
+    ): ResponseEntity<BaseResponse>
+}
