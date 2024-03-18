303dc7b60135ca2db2fe63cb61ff1ef5fd87a9d2
XiaoLFeng <gm@x-lf.cn>
2024-03-18 22:01:31 +0800

    patch: 合并冲突补丁

--- src/main/kotlin/com/frontleaves/greenchaincarbonledger/services/impl/CarbonServiceimpl.java
+++ src/main/kotlin/com/frontleaves/greenchaincarbonledger/services/impl/CarbonServiceimpl.java
@@ -1,8 +1,15 @@
 package com.frontleaves.greenchaincarbonledger.services.impl;
 
 import com.frontleaves.greenchaincarbonledger.dao.CarbonAccountingDAO;
+import com.frontleaves.greenchaincarbonledger.dao.CarbonDAO;
+import com.frontleaves.greenchaincarbonledger.dao.UserDAO;
 import com.frontleaves.greenchaincarbonledger.models.doData.CarbonAccountingDO;
+import com.frontleaves.greenchaincarbonledger.models.doData.CarbonQuotaDO;
+import com.frontleaves.greenchaincarbonledger.models.doData.CarbonReportDO;
+import com.frontleaves.greenchaincarbonledger.models.doData.UserDO;
 import com.frontleaves.greenchaincarbonledger.models.voData.returnData.BackCarbonAccountingVO;
+import com.frontleaves.greenchaincarbonledger.models.voData.returnData.BackCarbonQuotaVO;
+import com.frontleaves.greenchaincarbonledger.models.voData.returnData.BackCarbonReportVO;
 import com.frontleaves.greenchaincarbonledger.services.CarbonService;
 import com.frontleaves.greenchaincarbonledger.utils.BaseResponse;
 import com.frontleaves.greenchaincarbonledger.utils.ErrorCode;
@@ -16,34 +23,121 @@ import org.jetbrains.annotations.Nullable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- * @author 32841
+ * @author FLASHLACK
  */
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class CarbonServiceimpl implements CarbonService {
+public class CarbonServiceImpl implements CarbonService {
+    private final CarbonDAO carbonDAO;
+    private final UserDAO userDAO;
     private final CarbonAccountingDAO carbonAccountingDAO;
 
     @NotNull
     @Override
-    public ResponseEntity<BaseResponse> getOwnCarbonQuota(long timestamp, @NotNull HttpServletRequest request, @Nullable Integer start, @Nullable Integer end) {
-        log.info("[Service]执行  getOwnCarbonQuota 操作");
-        //先将此UUID进行数据库校验
-        return null;
+    public ResponseEntity<BaseResponse> getOwnCarbonQuota(long timestamp, @NotNull HttpServletRequest request, String start, @Nullable String end) {
+        log.info("[Service] 执行 getOwnCarbonQuota 方法");
+        UserDO getAuthUserDO = ProcessingUtil.getUserByHeaderUuid(request, userDAO);
+        if (getAuthUserDO != null) {
+            //检查数据
+            if (start == null || start.isEmpty()) {
+                start = new SimpleDateFormat("yyyy").format(getAuthUserDO.getCreatedAt());
+            }
+            // 数据库读取数据
+            ArrayList<CarbonQuotaDO> carbonQuotaList = carbonDAO.getQuotaListByOrganizeUuid(getAuthUserDO.getUuid(), start, end);
+            ArrayList<BackCarbonQuotaVO> backCarbonQuotaVOList = new ArrayList<>();
+            carbonQuotaList.forEach(carbonQuotaDO -> {
+                BackCarbonQuotaVO backCarbonQuotaVO = new BackCarbonQuotaVO();
+                backCarbonQuotaVO
+                        .setUuid(carbonQuotaDO.getUuid())
+                        .setOrganizeUuid(carbonQuotaDO.organizeUuid)
+                        .setQuotaYear(carbonQuotaDO.quotaYear)
+                        .setTotalQuota(carbonQuotaDO.totalQuota)
+                        .setAllocatedQuota(carbonQuotaDO.getAllocatedQuota())
+                        .setUsedQuota(carbonQuotaDO.usedQuota)
+                        .setAllocationDate(carbonQuotaDO.allocationDate)
+                        .setComplianceStatus(carbonQuotaDO.complianceStatus)
+                        .setCreatedAt(carbonQuotaDO.createdAt)
+                        .setUpdatedAt(carbonQuotaDO.updatedAt);
+                backCarbonQuotaVOList.add(backCarbonQuotaVO);
+            });
+            return ResultUtil.success(timestamp, backCarbonQuotaVOList);
+        } else {
+            return ResultUtil.error(timestamp, ErrorCode.USER_NOT_EXISTED);
+        }
     }
 
     @NotNull
     @Override
-    public ResponseEntity<BaseResponse> getCarbonAccounting(long timestamp, @NotNull HttpServletRequest request, Integer limit, Integer page, String order) {
+    public ResponseEntity<BaseResponse> getCarbonReport(
+            long timestamp,
+            @NotNull HttpServletRequest request, @NotNull String type, @NotNull String search, @NotNull String limit,
+            @NotNull String page, String order
+    ) {
+        log.info("[Service] 执行 getCarbonReport 方法");
+        String getUuid = ProcessingUtil.getAuthorizeUserUuid(request);
+        //校验组织是否在系统中进行碳核算
+        List<CarbonAccountingDO> getAccountList = carbonDAO.getAccountByUuid(getUuid);
+        if (!getAccountList.isEmpty()) {
+            // 检查参数，如果未设置（即为null），则使用默认值
+            limit = (limit.isEmpty() || Integer.parseInt(limit) > 100) ? "20" : limit;
+            page = (page.isEmpty()) ? "1" : page;
+            if (order.isBlank()) {
+                order = "ASC";
+            }
+            log.debug("\t> limit: {}, page: {}, order: {}", limit, page, order);
+            //进行type值判断
+            List<CarbonReportDO> getReportList;
+            order = "id " + order;
+            switch (type) {
+                case "all" -> getReportList = carbonDAO.getReportByUuid(getUuid, limit, page, order);
+                case "search" -> getReportList = carbonDAO.getReportBySearch(getUuid, search, limit, page, order);
+                case "draft", "pending_review", "approved", "rejected" ->
+                        getReportList = carbonDAO.getReportByStatus(getUuid, search, limit, page, order);
+                default -> {
+                    return ResultUtil.error(timestamp, "type 参数有误", ErrorCode.REQUEST_BODY_ERROR);
+                }
+            }
+            //整理数据
+            ArrayList<BackCarbonReportVO> backCarbonReportList = new ArrayList<>();
+            if (getReportList != null) {
+                for (CarbonReportDO getReport : getReportList) {
+                    BackCarbonReportVO backCarbonReportVO = new BackCarbonReportVO();
+                    backCarbonReportVO
+                            .setId(getReport.getId())
+                            .setOrganizeUuid(getReport.getOrganizeUuid())
+                            .setAccountingPeriod(getReport.getAccountingPeriod())
+                            .setTotalEmission(getReport.getTotalEmission())
+                            .setEmissionReduction(getReport.getEmissionReduction())
+                            .setNetEmission(getReport.getNetEmission())
+                            .setReportStatus(getReport.getReportStatus())
+                            .setCreatedAt(getReport.getCreatedAt())
+                            .setUpdatedAt(getReport.getUpdateAt());
+                    backCarbonReportList.add(backCarbonReportVO);
+                }
+                //输出
+                return ResultUtil.success(timestamp, "获取自己组织碳核算报告信息已准备完毕", backCarbonReportList);
+            } else {
+                return ResultUtil.error(timestamp, ErrorCode.SERVER_INTERNAL_ERROR);
+            }
+        } else {
+            return ResultUtil.error(timestamp, ErrorCode.CAN_T_ACCOUNT_FOR_CARBON);
+        }
+    }
+
+    @NotNull
+    @Override
+    public ResponseEntity<BaseResponse> getCarbonAccounting(long timestamp, @NotNull HttpServletRequest request, String limit, String page, @NotNull String order) {
         log.info("[Service] 执行 getCarbonAccounting 方法");
         // 检查参数，如果未设置（即为null），则使用默认值
-        limit = (limit == null || limit > 100) ? 20 : limit;
-        page = (page == null) ? 1 : page;
-        if (order == null || order.isBlank()) {
+        limit = (limit.isEmpty() || Integer.parseInt(limit) > 100) ? "20" : limit;
+        page = (page.isEmpty()) ? "1" : page;
+        if (order.isBlank()) {
             order = "id ASC";
         } else {
             order = "id " + order;
@@ -51,7 +145,7 @@ public class CarbonServiceimpl implements CarbonService {
         // 获取自己企业的Uuid
         String organizeUuid = ProcessingUtil.getAuthorizeUserUuid(request);
         List<BackCarbonAccountingVO> carbonAccountinglist = new ArrayList<>();
-        List<CarbonAccountingDO> carbonAccountingDOList = carbonAccountingDAO.getCarbonAccountingList(organizeUuid, limit, page, order);
+        List<CarbonAccountingDO> carbonAccountingDOList = carbonAccountingDAO.getCarbonAccountingList(organizeUuid, Integer.parseInt(limit), Integer.parseInt(page), order);
         if (carbonAccountingDOList != null){
             for (CarbonAccountingDO carbonAccountingDO : carbonAccountingDOList) {
                 BackCarbonAccountingVO backCarbonAccountingVO = new BackCarbonAccountingVO();
@@ -63,14 +157,13 @@ public class CarbonServiceimpl implements CarbonService {
                         .setAccountingPeriod(carbonAccountingDO.getAccountingPeriod())
                         .setEmissionSource(carbonAccountingDO.getEmissionSource())
                         .setDataVerificationStatus(carbonAccountingDO.getDataVerificationStatus())
-                        .setCreateAt(carbonAccountingDO.getCreateAt())
-                        .setUpdateAt(carbonAccountingDO.getUpdateAt());
+                        .setCreateAt(carbonAccountingDO.getCreatedAt())
+                        .setUpdateAt(carbonAccountingDO.getUpdatedAt());
                 carbonAccountinglist.add(backCarbonAccountingVO);
             }
             return ResultUtil.success(timestamp, "数据已准备完毕", carbonAccountinglist);
         } else {
             return ResultUtil.error(timestamp, ErrorCode.SELECT_DATA_ERROR);
         }
-
     }
 }
