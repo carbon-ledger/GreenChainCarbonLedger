8d671df19da345fd5234cf043fb9fdaef5b06e29
FLASHLACK1314 <zlp18021547586@163.com>
2024-03-24 16:52:56 +0800

    PATH-CarbonTypeMapper

---
+++ src/main/kotlin/com/frontleaves/greenchaincarbonledger/mappers/CarbonTypeMapper.java
@@ -0,0 +1,12 @@
+package com.frontleaves.greenchaincarbonledger.mappers;
+
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 对于CarbonType表数据库操作语句
+ * @author FALSHLACK
+ */
+@Mapper
+public interface CarbonTypeMapper {
+
+}
