d1cde764245138f6f2ee7f30984a08709ab0f687
FLASHLACK1314 <zlp18021547586@163.com>
2024-03-24 17:02:23 +0800

    CarbonTypeDAO和CarbonMapper的创建

--- src/main/kotlin/com/frontleaves/greenchaincarbonledger/mappers/CarbonTypeMapper.java
+++ src/main/kotlin/com/frontleaves/greenchaincarbonledger/mappers/CarbonTypeMapper.java
@@ -1,6 +1,8 @@
 package com.frontleaves.greenchaincarbonledger.mappers;
 
+import com.frontleaves.greenchaincarbonledger.models.doData.CarbonTypeDO;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 /**
  * 对于CarbonType表数据库操作语句
@@ -8,5 +10,6 @@ import org.apache.ibatis.annotations.Mapper;
  */
 @Mapper
 public interface CarbonTypeMapper {
-
+    @Select("SELECT * FROM fy_carbon_type WHERE name=#{}")
+    CarbonTypeDO getTypeByName(String name);
 }
