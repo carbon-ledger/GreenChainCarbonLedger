ce6502d7c9fd338175207a7efeaa7d42192498d3
DC_DC <3284155190@qq.com>
2024-03-31 14:40:31 +0800

    patch-完善tradeReleaseVO实体类，校验参数注解

--- src/main/kotlin/com/frontleaves/greenchaincarbonledger/models/voData/getData/TradeReleaseVO.java
+++ src/main/kotlin/com/frontleaves/greenchaincarbonledger/models/voData/getData/TradeReleaseVO.java
@@ -1,5 +1,6 @@
 package com.frontleaves.greenchaincarbonledger.models.voData.getData;
 
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 /**
@@ -7,7 +8,9 @@ import lombok.Data;
  */
 @Data
 public class TradeReleaseVO {
+    @Pattern(regexp = "\\d+(\\.\\d{0,2})?$", message = "必须为正数，且最多两位小数")
     String amount;
+    @Pattern(regexp = "\\d+(\\.\\d{0,2})?$", message = "必须为正数，且最多两位小数")
     String unit;
     String text;
     Boolean draft;
